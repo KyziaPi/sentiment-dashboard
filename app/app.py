82669de5afbb9c9f2c76bc1a319406ed2e7300c9
Ubuntu <yeye@GuecoYerikaHome.j02mnivizzzu3amswgtysc2lfh.hx.internal.cloudapp.net>
2026-02-03 01:50:36 +0000

    Update to work online

--- app/app.py
+++ app/app.py
@@ -48,4 +48,4 @@ def index():
     return render_template("index.html", results=results, country=country)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
